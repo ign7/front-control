675047ba82bb7a168bf367a6ee35a4f0128aceb3
igor <quaresmaigor@yahoo.com>
2023-07-17 15:16:39 -0300

    manut

--- src/verVeiculo/Veiculo.jsx
+++ src/verVeiculo/Veiculo.jsx
@@ -15,8 +15,8 @@ function Veiculo() {
 
   const [listVeiculos, guardaVeiculos] = useState([]);
 
-  const urlimg=require.context('../images/', true);
- 
+  const urlimg = require.context('../images/', true);
+
 
 
   function getVeiculos() {
@@ -44,12 +44,12 @@ function Veiculo() {
         </div>
 
 
-        <div className='col-10' >         
+        <div className='col-10' >
           {listVeiculos.map(Veiculos => (
-            <div className="bloco"  >
+            <div className="bloco">
               <div className="body-bloco-list" >
                 <h2 className='tveiculo'>{Veiculos.marca} {Veiculos.nome}</h2>
-                <img src={urlimg(`./${Veiculos.imagem}`)}  style={{ width: 15 + 'rem', height: 50 + '%' }} class="img-thumbnail" ></img>
+                <img src={urlimg(`./${Veiculos.imagem}`)} style={{ width: 15 + 'rem', height: 50 + '%' }} class="img-thumbnail" ></img>
                 <span className='place'>Placa : {Veiculos.placa}</span>
                 <span className='year'>Ano de Fabricação: {Veiculos.ano}</span>
               </div>
@@ -58,14 +58,62 @@ function Veiculo() {
                   <button onClick={getVeiculos} class="btn btn-danger">Detalhes</button>
                 </div>
                 <div className='manut'>
-                  <button class="btn btn-warning">Manutenção</button>
+                  <button data-bs-toggle="modal" data-bs-target="#manutencaomodal" class="btn btn-warning">Manutenção</button>
                 </div>
               </div>
-            </div>                    
-          ))          
-          }                        
-        </div>      
+            </div>
+          ))
+          }
+        </div>
       </div>
+
+
+      <div class="modal fade" id="manutencaomodal" tabindex="-1" aria-labelledby="manutencaomodalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-2" id="manutencaomodalLabel">Manutençoes <span class="badge bg-warning">Veiculares</span></h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+              <form class="row g-3" >
+                <div class="col-md-6">
+                  <label for="inputnome4" class="form-label">Nome</label>
+                  <input type="text" name="nome" class="form-control" id="inputnome23" />
+                </div>
+                <div class="col-md-6">
+                  <label for="inputkm4" class="form-label">Km Veiculo </label>
+                  <input type="number" name="km" class="form-control" id="inputkm23" />
+                </div>
+                <div class="col-md-6">
+                  <label for="inputvalor" class="form-label">valor</label>
+                  <input type="number" name="valor" class="form-control" id="inputvalor23" />
+                </div>
+                <div class="col-6">
+                  <label for="inputPlaca" class="form-label">Distancia</label>
+                  <input type="number" name="distancia" class="form-control" id="input23" />
+                </div>
+                <div class="col-md-6">
+                  <label htmlFor="inputAno" className="form-label">Data Manutenção</label>
+                  <input type="date" name="dataSolicitacao" class="form-control" id="inputo23" />
+                </div>
+                <div class="col-6">
+                  <label for="inputobs2" class="form-label">Observação</label>
+                  <input type="text" name="obs" class="form-control" id="obs" />
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button class="btn btn-warning" data-bs-dismiss="modal"> Save </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
 
   );
